687522156fbe19556c87c8ca17f9c6380ed1427f
Babou <148575592+WylexxelyW@users.noreply.github.com>
2026-01-15 22:26:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,28 +4,35 @@ import sqlite3
 from Bam import creer_base, con, cur
 
 def test_creer_base():
-    
+    print("Tests de la fonction creer_base() :")
     creer_base(9, 0, 18, 0, 50, 50)
     
     cur.execute("""SELECT * FROM boutique_location""")
     resultat = cur.fetchone()
-    #Cas normal
+    
+    # ---Cas normal---
+    
     assert resultat == (9, 0, 18, 0, 50, 50), 'Pas ok'
     print("✓")
-    #cas où nb de kayaks négatif
+    
+    # ---Cas où nb de kayaks négatif---
+    print("Test 2 : ")
     try : 
-        assert resultat == (9, 0, 18, 0, 50, -50), 'Pas ok'
+        assert resultat == (9, 0, 18, 0, 50, -50), '✕'
+        print(" ✓")
+    except:
+        print("✕")
+        
+    #---Cas ou l'heure d'ouverture est après l'heure de fermeture---
+    print("Test 3 : ", end = "")
+    try : 
+        assert resultat == (18, 0, 9, 0, 50, 50), '✕'
         print("✓")
     except:
-        print("Pa s ok")
+        print("✕")
         
-    #
-    try : 
-        assert resultat == (18, 0, 9, 0, 50, 50), 'Pas ok'
-        print("✓")
-    except:
-        print("Pas ok")
         
+
 if __name__ == "__main__":
     try:
         test_creer_base()
