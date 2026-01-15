17bca489b55a62901f686e89d36ac5bd77051552
Your Name <you@example.com>
2026-01-15 15:23:06 +0100

    so long kayak

--- Bam2peut-etre.py
+++ Bam2peut-etre.py
@@ -367,19 +367,6 @@ def retour_kayaks1place(j_depart : int, m_depart : int, a_depart : int) :
     # chaque 3-uplets : (heure, minute, nb_kayaks à ramasser)
     
 def kayak_dispo(j_depart : int, m_depart : int, a_depart : int, h_depart : int, min_depart : int, nb_1place : int, nb_2places : int, parcours : int) -> bool :
-    cur.execute(f"""SELECT nb_1place FROM location WHERE j_depart = {j_depart} AND m_depart = {m_depart} AND a_depart = {a_depart} 
-                AND h_depart > {h_depart-parcours} AND h_depart+{parcours} < {h_depart+parcours} AND min_depart = {min_depart}""")
-        #min_depart doit probablement être modifiée comme h_depart, j'ai un peu de mal à voir comment pour l'instant
-        # je comprend pas où tu veux aller mais c'est sûrement faux
-    
-
-
-
-
-# j'ai essayé de trouver une méthode pour guider ton travail ---->
-
-
-
     cur.execute(f"""SELECT SUM(nb_1place) FROM location WHERE j_depart = {j_depart} AND m_depart = {m_depart} AND a_depart = {a_depart} 
                 AND (h_depart < {h_depart} OR (h_depart = {h_depart} AND min_depart = {min_depart}))""")
     # on selectionne toutes les resa kayak une place et les somme
